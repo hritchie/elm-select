2be92231509e669d4eb4c8886136107ed2a0178e
Heath Ritchie <heath.ritchie@gmail.com>
2021-11-24 12:59:25 -0500

    delete js files

--- examples-optimized/index.ts
+++
@@ -1,8 +0,0 @@
-//@ts-ignore
-import { Elm } from "./Main.elm";
-import "@confidenceman02/elm-select";
-
-const app = Elm.Main.init({
-  node: document.querySelector("main"),
-  flags: null,
-});
--- src/viewObservers.ts
+++
@@ -1,6 +0,0 @@
-import { DefoViews } from "./types";
-import { dynamicSelectInput } from "./dynamicSelectInput";
-
-export const viewObservers: DefoViews = {
-  dynamicSelectInput: dynamicSelectInput,
-};
